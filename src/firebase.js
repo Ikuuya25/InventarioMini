3d2752c73c508a42862fcc1a853382cba60b7e7d
Sebastian Grez <grezsebastian6@gmail.com>
2026-02-20 19:55:42 -0300

    Clear Firebase configuration values
    Removed sensitive Firebase configuration details.

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore"; // ← IMPORTANTE
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCt5ySU4kmoxqViJd0QAhr9zQatYWiTzC0",
-  authDomain: "inventariominilarrytech-82581.firebaseapp.com",
-  projectId: "inventariominilarrytech-82581",
-  storageBucket: "inventariominilarrytech-82581.firebasestorage.app",
-  messagingSenderId: "1006545291534",
-  appId: "1:1006545291534:web:c28a8189fac978a0bd2f5f",
-  measurementId: "G-QSBS7R865G"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
